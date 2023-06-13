7de9724b2b89bd2955c924bfd76fb0eb21295abf
kapil0078 <kapilkatrodiya125@gmail.com>
2023-06-13 19:42:53 +0530

    API-1

---
+++ lib/HelperFunction/my_nevigation.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+
+
+nextScreen(BuildContext context, Widget page) async {
+  await Navigator.push(
+    context,
+    CupertinoPageRoute(
+      builder: (context) => page,
+    ),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:api_calling/BasicLevel/api_calling_1.dart';
+import 'package:api_calling/HelperFunction/my_nevigation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'API',
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.brown.shade500),
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.brown.shade300,
+          elevation: 0.0,
+          centerTitle: true,
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("API Calling"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                nextScreen(
+                  context,
+                  const APICalling1(),
+                );
+              },
+              child: const Text("API-1"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

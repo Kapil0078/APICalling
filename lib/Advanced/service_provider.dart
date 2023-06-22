a7c4aae204dac6062a893fdf904274d5492b5742
kapil0078 <kapilkatrodiya125@gmail.com>
2023-06-22 19:45:49 +0530

    API with StateManagement

---
+++ lib/Advanced/service_provider.dart
@@ -0,0 +1,59 @@
+import 'dart:developer';
+
+import 'package:api_calling/Advanced/responce_class.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class ServiceProvider extends ChangeNotifier {
+  final Dio dio = Dio();
+
+  bool isLoadingForService = false;
+  Future<ResponseClass> readService() async {
+    const uri = "https://garage.logispire.in/api/service/garage/1";
+
+    ResponseClass responseClass = ResponseClass(
+      message: "API is Calling",
+      success: false,
+    );
+    try {
+      isLoadingForService = true;
+      notifyListeners();
+      Response response = await dio.get(
+        uri,
+        options: Options(
+          validateStatus: (status) {
+            return status == 200 ||
+                status == 400 ||
+                status == 404 ||
+                status == 500;
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        responseClass.success = true;
+        responseClass.message = response.data['msg'];
+        responseClass.data = response.data['data'];
+        isLoadingForService = false;
+        notifyListeners();
+        return responseClass;
+      } else {
+        responseClass.success = false;
+        responseClass.message = "Somthing Went Wrong";
+        isLoadingForService = false;
+        notifyListeners();
+        return responseClass;
+      }
+    } on DioException catch (e) {
+      log("DioCatchError : $e");
+      isLoadingForService = false;
+      notifyListeners();
+      return responseClass;
+    } catch (e) {
+      log("CatchError : $e");
+      isLoadingForService = false;
+      notifyListeners();
+      return responseClass;
+    }
+  }
+}
